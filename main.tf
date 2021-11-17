9269ef91549160f0c9bdbe23cf4ba9d5ba09d0fa
Ryan Ryke <ryan@itsacloudlife.com>
2021-11-17 12:47:45 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,14 +95,10 @@ module "container_definition" {
       "awslogs-stream-prefix" = var.aws_logs_prefix == "" ? module.this.name : var.aws_logs_prefix
     }
     secretOptions = null
-  } : null
+  } : {logDriver = var.log_driver
+    options = {var.firelens_configuration}}
 
-  log_configuration = var.firelens_enabled ? {
-    logDriver = var.log_driver
-    options = var.firelens_configuration
-  }
-    secretOptions = null
-  } : null
+
 }
 
 locals {
