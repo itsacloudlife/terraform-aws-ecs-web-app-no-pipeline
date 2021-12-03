ba4ad3927ff8aa8824fd808e2ead1d98337e7511
Ryan Ryke <ryan@itsacloudlife.com>
2021-12-02 17:16:12 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ module "container_definition" {
 
 
   log_configuration = var.cloudwatch_log_group_enabled ? {
-    logDriver = "awslogs"
+    logDriver = awslogs
     options = {
       "awslogs-region"        = coalesce(var.aws_logs_region, data.aws_region.current.name)
       "awslogs-group"         = join("", aws_cloudwatch_log_group.app.*.name)
@@ -96,8 +96,8 @@ module "container_definition" {
     }
     secretOptions = null
   } : {
-    logDriver = "awsfirelens"
-      options = {var.firelens_configuration}
+    logDriver = awsfirelens
+      options = {}
 
   }
 
